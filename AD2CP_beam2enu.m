6b7d2f7c8b64685cfe4840eb99a2f69fadb15100
Sam Coakley <sam12396@gmail.com>
2019-03-12 12:14:03 -0400

    Add QA/QC steps to the beam conversion
    Flag data with return amplitude below SNR threshold.
    Remove data outside of pitch and roll ranges.

--- AD2CP_beam2enu.m
+++ AD2CP_beam2enu.m
@@ -20,6 +20,8 @@ clear all; close all;
 mission='ru31_467_2016_01_13';
 fname=[mission '_AD2CP.nc'];
 data_path=['/Users/samcoa/Documents/MATLAB/ADCP_Wave/Data/' mission '/' fname];
+fig_path=['/Users/samcoa/Documents/MATLAB/ADCP_Wave/Figures/' mission '/'];
+
 
 % These are the transformation matricies for up and down cast. Not sure
 % which is which so I need to find some documentation.
@@ -34,7 +36,7 @@ T124 = [1.35634170490926  -0.50552979193390  -0.50552979193390;...
                   0   0.55168895948125   0.55168895948125];
 
 % Get the bottom of each depth bin
-rng_cells=ncread(data_path,'Average_Range',[1,1],[1,15],[1,1])';
+rng_cells=ncread(data_path,'Average_Range',[1,1],[1,15])';
 
 % Get the time variable
 avg_time=ncread(data_path,'Average_MatlabTimeStamp');
@@ -46,60 +48,193 @@ v3=double(ncread(data_path,'Average_VelBeam3')); % Rear X beam
 v4=double(ncread(data_path,'Average_VelBeam4'));
 
 % Get magnetically corrected behavior data
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Do I need a mag corr for pitch, roll ?
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 head=double(ncread(data_path,'Average_Heading_Mag_Corr'));
 pitch=double(ncread(data_path,'Average_Pitch'));
 roll=double(ncread(data_path,'Average_Roll'));
 pressure=double(ncread(data_path,'Average_Pressure'));
 ensemble=double(ncread(data_path,'Average_EnsembleCount'));
 
+%% Beam specific QA/QC
+% Some quality control methods require beam velocities and thus, they need
+% to be done before the conversion to ENU velocities. We do not need to
+% worry about Correlation values because we are using the average data set
+
+%% Beam Amplitude QA/QC
+% Remove any bin and bin further away from it that returns an amplitude
+% within 3dB of the noise floor. We also need to calculate the noise floor.
+% This method is recommended by the manual
+% Todd used a different method. Use SNR to do a similar check but say any
+% bin with SNR<20 is bad
+ampbeam1=double(ncread(data_path,'Average_AmpBeam1'));
+ampbeam2=double(ncread(data_path,'Average_AmpBeam2'));
+ampbeam3=double(ncread(data_path,'Average_AmpBeam3'));
+ampbeam4=double(ncread(data_path,'Average_AmpBeam4'));
+
+% Define the noise floor
+nf=25; %dB. Reported by Nortek by sampling in air 
+
+% SNR_dB=S_dB-N_dB use 13dB as theshold (Todd 2017). 
+snr_threshold=13;
+for ii=1:size(ampbeam1,1)
+    if pitch(ii)>15 % Upcast so use beams 234
+        % Calculate SNR for each beam
+        snr2=(ampbeam2(ii,:)-nf);
+        snr3=(ampbeam3(ii,:)-nf);
+        snr4=(ampbeam4(ii,:)-nf);
+        
+        % If the value of SNR < threshold in a bin in any beam, make that
+        % bin nan in all beams needed for this cast
+        snr_ind=snr2<snr_threshold | snr3<snr_threshold | snr4<snr_threshold;
+        ampbeam2(ii,snr_ind)=NaN;
+        ampbeam3(ii,snr_ind)=NaN;
+        ampbeam4(ii,snr_ind)=NaN;
+    elseif pitch(ii)<-15 % Downcast so use beams 124
+        % Calculate SNR for each beam
+        snr1=(ampbeam1(ii,:)-nf);
+        snr2=(ampbeam2(ii,:)-nf); 
+        snr4=(ampbeam4(ii,:)-nf);
+        
+        % If the value of SNR < threshold in a bin in any beam, make that
+        % bin nan in all beams needed for this cast        
+        snr_ind=snr1<snr_threshold | snr2<snr_threshold | snr4<snr_threshold;
+        ampbeam1(ii,snr_ind)=NaN;
+        ampbeam2(ii,snr_ind)=NaN;
+        ampbeam4(ii,snr_ind)=NaN;
+    end
+    if ii==floor(size(ampbeam1,1)*0.25)
+        disp('25% of the way through SNR check')
+    elseif ii==floor(size(ampbeam1,1)*0.50)
+        disp('50% of the way through SNR check')
+    elseif ii==floor(size(ampbeam1,1)*0.75)
+        disp('75% of the way through SNR check')
+    end
+    clear snr_ind snr1 snr2 snr3 snr4
+end
+
+% Check how much data we lost in each beam
+a=[];
+for ii=1:size(ampbeam2,2)
+    a(1,ii)=sum(isnan(ampbeam1(:,ii)))./length(ampbeam1).*100;
+    a(2,ii)=sum(isnan(ampbeam2(:,ii)))./length(ampbeam1).*100;
+    a(3,ii)=sum(isnan(ampbeam3(:,ii)))./length(ampbeam1).*100;
+    a(4,ii)=sum(isnan(ampbeam4(:,ii)))./length(ampbeam1).*100;
+end
+figure(1)
+hold on
+scatter(1:15,a(1,:),'r');plot(1:15,a(1,:),'r');
+scatter(1:15,a(2,:),'g');plot(1:15,a(2,:),'g');
+scatter(1:15,a(3,:),'k');plot(1:15,a(3,:),'k');
+scatter(1:15,a(4,:),'b');plot(1:15,a(4,:),'b');
+title(['# of Nans by bins across all ensembles from SNR threshold of: ' num2str(snr_threshold) 'dB and Noise Floor of: ' num2str(nf) 'dB'])
+legend('Beam1','Beam1','Beam2','Beam2','Beam3','Beam3','Beam4','Beam4','Location','NW')
+ylabel('% NaN in data')
+xlabel('Bin number')
+print([fig_path 'nans_from_snr_threshold_' num2str(snr_threshold) '_dB'],'-dpng')
+clear a 
+%close all
+
+% Remove extreme amplitude returns
+% Set extreme threshold
+inter_level=75;
+for ii=1:size(ampbeam1,1)
+    if pitch(ii)>15 % Upcast so use beams 234
+        % If the return is above the threshold, flag the data
+        amp2_ind= ampbeam2(ii,:)>inter_level;
+        amp3_ind= ampbeam3(ii,:)>inter_level;
+        amp4_ind= ampbeam4(ii,:)>inter_level;
+        amp_ind=amp2_ind+amp3_ind+amp4_ind;
+        ampbeam2(amp_ind~=0,:)=NaN;
+        ampbeam3(amp_ind~=0,:)=NaN;
+        ampbeam4(amp_ind~=0,:)=NaN;
+        
+    elseif pitch(ii)<-15 % Downcast so use beams 124
+        % If the return is above the threshold, flag the data
+        amp1_ind= ampbeam1(ii,:)>inter_level;
+        amp2_ind= ampbeam2(ii,:)>inter_level;
+        amp4_ind= ampbeam4(ii,:)>inter_level;
+        amp_ind=amp1_ind+amp2_ind+amp4_ind;
+        ampbeam1(amp_ind~=0,:)=NaN;
+        ampbeam2(amp_ind~=0,:)=NaN;
+        ampbeam4(amp_ind~=0,:)=NaN;
+    end
+    if ii==floor(size(ampbeam1,1)*0.25)
+        disp('25% of the way through SNR check')
+    elseif ii==floor(size(ampbeam1,1)*0.50)
+        disp('50% of the way through SNR check')
+    elseif ii==floor(size(ampbeam1,1)*0.75)
+        disp('75% of the way through SNR check')
+    end
+    clear amp_ind amp1_ind amp2_ind amp3_ind amp4_ind
+end
+%% Apply qa/qc to actual beam velocities
+
+qaqc=input('Perform QA/QC on the velocity data? Y=1 N=0');
+
+if qaqc==1
+    qaqc1_ind=isnan(ampbeam1);
+    qaqc2_ind=isnan(ampbeam2);
+    qaqc3_ind=isnan(ampbeam3);
+    qaqc4_ind=isnan(ampbeam4);
+
+    v1(qaqc1_ind)=NaN;
+    v2(qaqc2_ind)=NaN;
+    v3(qaqc3_ind)=NaN;
+    v4(qaqc4_ind)=NaN;
+    disp('QA/QC complete')
+else
+    disp('QA/QC skipped')
+end
+
 %% Convert beam velocity to ENU velocity
 
+% Do pitch roll if statements outside of the for loop and trim data that
+% way the loop doesnt take as long. Only take pitchs that indicate they
+% are during a dive (>+-15). Only take points with roll inside +-10.
+trim_ind= abs(pitch)>15 & abs(roll)<10;
+trim_pitch=pitch(trim_ind);
+trim_roll=roll(trim_ind);
+trim_head=head(trim_ind);
+trim_avg_time=avg_time(trim_ind);
+trim_v1=v1(trim_ind,:);
+trim_v2=v2(trim_ind,:);
+trim_v3=v3(trim_ind,:);
+trim_v4=v4(trim_ind,:);
+trim_pressure=pressure(trim_ind);
+
 kk=1;
-for k=1:length(avg_time) %loop through each ensemble
+for k=1:length(trim_avg_time) %loop through each ensemble
     
-V1=v1(k,:);
-V2=v2(k,:);
-V3=v3(k,:);
-V4=v4(k,:);
+V1=trim_v1(k,:);
+V2=trim_v2(k,:);
+V3=trim_v3(k,:);
+V4=trim_v4(k,:);
 
 % Concat all up/down cast beam data
 beam124 = [V1;V2;V4];
 beam234 = [V2;V3;V4];
-
-% Convert the beam velocity data to ENU velocity data
-% Is the setting of 1 for the last argument correct? I do not understand
-% that parameter. Why was the bottom track 0
-[enu124] = Xform(beam124,T124,head(k),pitch(k),roll(k),1);
-[enu234] = Xform(beam234,T234,head(k),pitch(k),roll(k),1);
-
+    
 % Calculate the vertical displacement of the bins adjusting for pitch and 
 % roll
-zrng = cell_vert(pitch(k),roll(k),rng_cells); 
-zrng = pressure(k) + zrng; % this is a real bin depth now
+zrng = cell_vert(trim_pitch(k),trim_roll(k),rng_cells); 
+zrng = trim_pressure(k) + zrng; % this is a real bin depth now
 
-            % Creates a field with the real bin depth
-            X.r(kk,:) = zrng.'; 
-            % Change the beam data being used for up or down cast. The beam
-            % orientation is 1=Forward; 2 and 4=Port and Starboard; 3= Aft.
-            % +-15° removes data from when the glider is inflecting. Skip
-            % anything with too great of a roll.
-            if pitch(k) < -15 && abs(roll(k))>10
-            X.e(kk,:)=enu124(1,:);
-            X.n(kk,:)=enu124(2,:);
-            X.u(kk,:)=enu124(3,:);
-            elseif pitch(k) > 15 && abs(roll(k))>10
-            X.e(kk,:)=enu234(1,:);
-            X.n(kk,:)=enu234(2,:);
-            X.u(kk,:)=enu234(3,:);
-            else
-            X.e(kk,:)=nan(size(enu124));
-            X.n(kk,:)=nan(size(enu124));
-            X.u(kk,:)=nan(size(enu124));                
-            end
+% Convert the beam velocity data to ENU velocity data.
+% Only convert the front beams if downcast and back beams for upcast
+% Is the setting of 1 for the last argument correct? I do not understand
+% that parameter. Why was the bottom track 0
+    if trim_pitch(k) < -15
+        [enu124] = Xform(beam124,T124,trim_head(k),trim_pitch(k),trim_roll(k),1);
+        X.r(kk,:) = zrng.';  % Creates a field with the real bin depth
+        X.e(kk,:)=enu124(1,:);
+        X.n(kk,:)=enu124(2,:);
+        X.u(kk,:)=enu124(3,:);        
+    elseif trim_pitch(k) > 15
+        [enu234] = Xform(beam234,T234,trim_head(k),trim_pitch(k),trim_roll(k),1);
+        X.r(kk,:) = zrng.';  % Creates a field with the real bin depth
+        X.e(kk,:)=enu234(1,:);
+        X.n(kk,:)=enu234(2,:);
+        X.u(kk,:)=enu234(3,:);        
+    end
 kk=kk+1;
 
     % Display progress through the loop
@@ -112,9 +247,12 @@ kk=kk+1;
     end
 end %end ensemble loop
 
+% Store trimmed time variable
+X.t=trim_avg_time;
+
 %% Write the converted data to the file
-ens_num=length(avg_time);
-bin_num=size(v1,2);
+ens_num=length(trim_avg_time);
+bin_num=size(trim_v1,2);
 
 % Stop variables from being created if this script has been run before and
 % the variables already exist in the file.
@@ -130,32 +268,38 @@ for ii=1:length(info.Variables)
     end
 end
 
+
 % Skip creation of the variables if they already exist in the file
 if create_vars~=1
     % Create/write the data from the structure to the .nc file
     nccreate(data_path,'Average_EVelocity','Dimensions',...
-        {'Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
+        {'Trimmed_Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
     nccreate(data_path,'Average_NVelocity','Dimensions',...
-        {'Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
+        {'Trimmed_Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
     nccreate(data_path,'Average_UVelocity','Dimensions',...
-        {'Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
+        {'Trimmed_Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
     nccreate(data_path,'Average_Depth','Dimensions',...
-        {'Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
+        {'Trimmed_Ensemble_number',ens_num,'Bins',bin_num},'Datatype','double');
+    nccreate(data_path,'Average_TrimMatlabTimeStamp','Dimensions',...
+        {'Trimmed_Ensemble_number',ens_num},'Datatype','double');    
 end
 
 ncwrite(data_path,'Average_EVelocity',X.e);
 ncwrite(data_path,'Average_NVelocity',X.n);
 ncwrite(data_path,'Average_UVelocity',X.u);
 ncwrite(data_path,'Average_Depth',X.r);
+ncwrite(data_path,'Average_TrimMatlabTimeStamp',X.t);
 
 % Add comments and units
 ncwriteatt(data_path,'Average_EVelocity','Units','m/s');
 ncwriteatt(data_path,'Average_NVelocity','Units','m/s');
 ncwriteatt(data_path,'Average_UVelocity','Units','m/s');
 ncwriteatt(data_path,'Average_Depth','Units','m')
+ncwriteatt(data_path,'Average_TrimMatlabTimeStamp','Units','days');
 
-ncwriteatt(data_path,'Average_EVelocity','Comments','East/West velocity. Converted from beam velocity using Xform.m');
-ncwriteatt(data_path,'Average_NVelocity','Comments','North/South velocity. Converted from beam velocity using Xform.m');
-ncwriteatt(data_path,'Average_UVelocity','Comments','Up/Down velocity. Converted from beam velocity using Xform.m');
+ncwriteatt(data_path,'Average_EVelocity','Comments',['East/West velocity. Converted from beam velocity using Xform.m. SNR threshold of ' num2str(snr_threshold) ' was used']);
+ncwriteatt(data_path,'Average_NVelocity','Comments',['North/South velocity. Converted from beam velocity using Xform.m. SNR threshold of ' num2str(snr_threshold) ' was used']);
+ncwriteatt(data_path,'Average_UVelocity','Comments',['Up/Down velocity. Converted from beam velocity using Xform.m. SNR threshold of ' num2str(snr_threshold) ' was used']);
 ncwriteatt(data_path,'Average_Depth','Comments','Actual depth of bottom of each bin. From AD2CP_beam2enu.m.')
+ncwriteatt(data_path,'Average_TrimMatlabTimeStamp','Comments','Matlab timestamps of profiles remaining after pitch/roll trimming');
 
