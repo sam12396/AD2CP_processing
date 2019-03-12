04c30217dcc0dc150faaee741ba8df22a9def390
Sam Coakley <sam12396@gmail.com>
2019-03-12 12:15:49 -0400

    Changed the way shear was calculated

--- AD2CP_shear_calc.m
+++ AD2CP_shear_calc.m
@@ -13,65 +13,33 @@ deployment='ru31_467_2016_01_13';
 data_path=['/Users/samcoa/Documents/MATLAB/ADCP_Wave/Data/' deployment '/'];
 fig_path=['/Users/samcoa/Documents/MATLAB/ADCP_Wave/Figures/' deployment '/'];
 
-data_file=[data_path deployment '_ocean_glider_velo.mat'];
+% Load the velcoity data to calculate shear
+data_file=[data_path deployment '_ocean_velo_grid.mat'];
 load(data_file);
 
-%% Split the ocean velocity profiles into E/W and N/S components
-% u=real(O_ls) to get real (E/W) component
-% v=imag(O_ls) to get imag (N/S) component
-u=real(O_ls);
-v=imag(O_ls);
-
 %% Calculate shear from ocean velocity
-% Find min and max depth range for the grid from bnew
-d_min=min(bnew);
-d_max=max(bnew);
-dz   =bnew(2)-bnew(1); % Vertical resolution of the profiles
 
-% Find the necessary number of depth bins
-bin_num=((d_max-d_min)./dz)+1; % Add 1 to account for d_min bin
-
-% Number of profile columns needed to fill with ocean velocity data
-% Each profile starts with a depth less than the depth recorded at the end
-% of the last profile.
-prof_num=sum(diff(bnew)<0)+1;
-
+% Get dimensions of grid
+bin_num=size(ugrid,1); 
+prof_num=size(ugrid,2);
+dz =grid_bin(2)-grid_bin(1);
 % Make a grid to populate with shear values
 % Create the grid
 usgrid=nan([bin_num prof_num]);
 vsgrid=nan([bin_num prof_num]);
 s_maggrid=nan([bin_num prof_num]); % Grid for shear magnitude
 
-% Create a reference array for bin depth index
-grid_bin=(d_min:dz:d_max)';
-
 % Loop through each profile and find the du/dz dv/dz and then populate a
 % grid
-profile_ind=[0; find(diff(bnew)<0)]+1; % Index of the start of each profile
-
-for ii=1:length(profile_ind)-1
-    if ~isnan(profile_ind(ii))
-        % Grabs one profile of data
-        prof_bins=bnew(profile_ind(ii):profile_ind(ii+1)-1)';
-        prof_u   =u(profile_ind(ii):profile_ind(ii+1)-1)';
-        prof_v   =v(profile_ind(ii):profile_ind(ii+1)-1)';
-        
-        % Calculate the shear in each direction
-        prof_us=diff(prof_u)./diff(prof_bins);
-        prof_vs=diff(prof_v)./diff(prof_bins);
-        prof_mags=sqrt(prof_us.^2 + prof_vs.^2); % Shear magnitude
-        
-        % Finds the depths to at which to insert this data into the grid
-        [prof_mindep, grid_start]=min(prof_bins);
-        [prof_maxdep, grid_end]=max(prof_bins);
+for ii=1:prof_num
+    prof_us=diff(ugrid(:,ii))./diff(grid_bin);
+    prof_vs=diff(vgrid(:,ii))./diff(grid_bin);
+    prof_mags=sqrt(prof_us.^2 + prof_vs.^2); % Shear magnitude
     
-        % Populate the grid with shear calculations
-        % Need to subtract one off the index because diff returns N-1 points
-        usgrid(grid_start:grid_end-1,ii)=prof_us;
-        vsgrid(grid_start:grid_end-1,ii)=prof_vs;
-        s_maggrid(grid_start:grid_end-1,ii)=prof_mags;
-    else
-    end
+    usgrid(1:end-1,ii)=prof_us;
+    vsgrid(1:end-1,ii)=prof_vs;
+    s_maggrid(1:end-1,ii)=prof_mags;
+
 end
 save([data_path deployment '_ocean_shear.mat'], 'usgrid', 'vsgrid', 's_maggrid', 'grid_bin');
 
@@ -82,7 +50,7 @@ tolerance=3; % standard deviation tolerance for the colorbar limits
 shear=[usgrid(:);vsgrid(:)];
 mshear=nanmean(shear);
 stdshear=nanstd(shear);
-clim=[mshear-tolerance.*stdshear mshear+tolerance.*stdshear];
+clim=[-(mshear+tolerance.*stdshear) mshear+tolerance.*stdshear]; % center color bar at 0
 
 % Set plotting constants
 cmap=cmocean('delta');
